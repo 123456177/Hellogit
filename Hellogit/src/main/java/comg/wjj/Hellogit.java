2b9287b71e17610dd455629602bbb0582bb57502
lenovo <123456>
2022-08-07 22:52:42 +0800

    分支添加

--- Hellogit/src/main/java/comg/wjj/Hellogit.java
+++ Hellogit/src/main/java/comg/wjj/Hellogit.java
@@ -5,5 +5,6 @@ public class Hellogit {
         System.out.println("欢迎来到git");
         System.out.println("更新1");
         System.out.println("创建分支完成");
+        System.out.println("分支添加");
     }
 }
